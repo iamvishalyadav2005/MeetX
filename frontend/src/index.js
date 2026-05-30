6871a1dff8eae2f59d22332347ea92f38d76d13a
vishal <vy941254@gmail.com>
2026-05-30 17:40:42 +0530

    fix: build successful with cross-env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import process from "process";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-window.process = process;
+// Safe process polyfill for simple-peer / readable-stream
+if (!window.process) {
+  window.process = { env: { NODE_ENV: "production" } };
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />  
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 reportWebVitals();
